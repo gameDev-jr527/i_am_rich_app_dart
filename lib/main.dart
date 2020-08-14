86dc28b0619df116af22f06bf328c03113fadea9
Anu-Marvelz <anumarvelz527@gmail.com>
2020-08-14 21:26:02 +0530

    Created an app model which is bug free.....

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(),
+    home: Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Center(
+          child: Text('I am Rich'),
+        ),
+        backgroundColor: Colors.black38,
+      ),
+      body: Center(
+          child: Image(image: AssetImage('images/art-3592111_1920.jpg'))),
+    ),
   ));
 }
